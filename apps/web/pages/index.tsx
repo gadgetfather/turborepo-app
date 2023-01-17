0cdc9a8b0f699f3270e3fce38c1edb648a12e140
akshay-copods <akshay.kamble@copods.co>
2023-01-17 07:08:54 +0530

    add readme

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,13 @@
 import { Button } from "ui";
 
+import { add } from "math-helper-rolling-thunder";
+ 
+
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <h2>{add(1, 2)}</h2>
       <Button />
     </div>
   );
